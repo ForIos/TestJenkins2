2423b8f4dad950f547d45ae74ed0de4445bf4589
USER <894143788@qq.com>
2016-08-21 01:01:16 +0800

    dd
    dd

---
+++ TestJenkins222/TestJenkins222/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestJenkins222
+//
+//  Created by USER on 16/8/21.
+//  Copyright © 2016年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
